ed7cb7999692efce469035383bb6468dcf8db6d3
Marc <meir.guggenheim@gmail.com>
2023-11-17 10:10:12 -0500

    added default_scope and methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+    default_scope { where.not(status: 'deleted') }
+
+    def soft_delete
+        update(deleted_at: Time.current)
+    end
+    
+    def restore
+        update(deleted_at: nil)
+    end
 end
